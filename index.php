0f84572b5926bfbaa93a08c6b2ebe06c3d959e09
EvgeniiKlepilin <evgenii.klepilin@gmail.com>
2018-04-12 11:54:58 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
 		require "item_classes.php";
 		
 		$name = "Evgenii Klepilin";
-		$email = "";
-		$phone = "";
-		$address = "";
+		$email = "email@gmail.com";
+		$phone = "+1234567890";
+		$address = "Greenland, Icy Pike City, Frozen Street, Igloo 13";
 		$links = array("https://github.com/EvgeniiKlepilin", "https://evgeniiklepilin.github.io/resumebuilder/");
 		
-		$header = new Header();
+		$header = new Header($name, $address, $phone, $email, $links);
 		$header->printHeader();
     ?>
   </body>
